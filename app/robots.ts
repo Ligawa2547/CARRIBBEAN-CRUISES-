e500bf94a72620760a1c25c23fcc446dd5a6014b
Diguok23 <addijenwriters001@gmail.com>
2025-11-25 02:51:52 -0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/login", "/admin/", "/dashboard/"],
     },
-    sitemap: "https://caribbeancruises.site/sitemap.xml",
+    sitemap: "https://www.nclsail.com/sitemap.xml",
   }
 }
